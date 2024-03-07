f266757e53ec037a792c40fae7ad8bfe214e6308
Vasile Toncu <vasile.toncu@ip-192-168-1-11.eu-west-1.compute.internal>
2024-03-07 17:53:35 +0200

    Added an example
    Author:    Vasile Toncu <14279771+vasiletoncu@users.noreply.github.com>
    Date:      Thu Mar 7 18:00:17 2024 +0200

---
+++ src/main/java/io/vasiletoncu/example/Main.java
@@ -0,0 +1,10 @@
+package io.vasiletoncu.example;
+
+/**
+ * Created by vasile.toncu on 07.03.2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
